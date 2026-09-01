2db52c7fa3ed418476324c5222b25eb4a9e8dbac
waghmaresanjana750-ctrl <waghmaresanjana750@gmail.com>
2026-09-01 23:41:10 +0530

    Update server.js
    Signed-off-by: waghmaresanjana750-ctrl <waghmaresanjana750@gmail.com>

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
